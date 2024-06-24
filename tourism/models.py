b98689d917dc446261b19ce31333e44845112e3b
BorisMiasnikov <Jason2297@mail.ru>
2024-06-24 21:41:22 +0300

    Создал БД

--- tourism/models.py
+++ tourism/models.py
@@ -12,14 +12,33 @@ class Users(models.Model):
 class Coords(models.Model):
     latitude = models.DecimalField(max_digits=10, decimal_places=8)
     longitude = models.DecimalField(max_digits=10, decimal_places=8)
-    hight = models.IntegerField(max_length=4)
+    hight = models.IntegerField()
 
 
+class Level(models.Model):
+    LEVEL = (
+        ('1А', '1А'),
+        ('2А', '2А'),
+        ('3А', '3А'),
+        ('1B', '1Б'),
+        ('2B', '2Б'),
+        ('3B', '3Б'),
+        ('3B*', '3Б*'),
 
+    )
+    winter = models.CharField(max_length=3, choices=LEVEL, null=True)
+    summer = models.CharField(max_length=3, choices=LEVEL, null=True)
+    autumn = models.CharField(max_length=3, choices=LEVEL, null=True)
+    spring = models.CharField(max_length=3, choices=LEVEL, null=True)
 
 
 class Pereval(models.Model):  # сущность объявления
     # атрибуты объявления
+    ststus_choices = (
+        ('pending', 'pending'),
+        ('accepted', 'accepted'),
+        ('rejected', 'rejected'),
+    )
     beauty_title = models.CharField(max_length=255, default="пер.")
     title = models.CharField(max_length=255)
     other_titles = models.CharField(max_length=255, null=True)
@@ -27,15 +46,11 @@ class Pereval(models.Model):  # сущность объявления
     add_time = models.DateTimeField(auto_now_add=True)
     users_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     coords_id = models.ForeignKey(Coords, on_delete=models.CASCADE)
+    level_id = models.ForeignKey(Level, on_delete=models.CASCADE)
+    status = models.CharField(choices=ststus_choices, default='new')
 
-class Level(models.Model):
-    winter = models.CharField(max_length=3, null=True)
-    summer = models.CharField(max_length=3, null=True)
-    autumn = models.CharField(max_length=3, null=True)
-    spring = models.CharField(max_length=3, null=True)
-    pereval_id = models.ForeignKey(Pereval, on_delete=models.CASCADE)
 
 class Imeges(models.Model):
     pereval_id = models.ForeignKey(Pereval, on_delete=models.CASCADE)
-    data = models.ImageField(upload_to='pereval/', nell=True, blank=True)
+    data = models.ImageField(upload_to='pereval/', null=True, blank=True)
     title = models.CharField(max_length=255, null=True)
