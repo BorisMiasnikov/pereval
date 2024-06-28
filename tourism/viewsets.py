3b1895f69c1c1f87fd5cc10d27f8b5f8cece7afa
BorisMiasnikov <Jason2297@mail.ru>
2024-06-28 23:13:25 +0300

    Проблемы с ключаи ид при вьюсете

--- tourism/viewsets.py
+++ tourism/viewsets.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework import permissions
+from rest_framework.response import Response
 
 from tourism.serializers import *
 from tourism.models import *
@@ -25,11 +26,29 @@ class PerevalViewset(viewsets.ModelViewSet):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
 
-    # def create(self, request, *args, **kwargs):
-    #     print(request)
-    #     print(args)
-    #     print(kwargs)
-    #     return self.request
+    def create(self, request, *args, **kwargs):
+        serializer = PerevalSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            print(serializer.data)
+            return Response({
+                'status': status.HTTP_200_OK,
+                'massage': "OK",
+                'id': serializer.data['id'],
+            })
+        if status.HTTP_400_BAD_REQUEST:
+            return Response({
+                'status': status.HTTP_400_BAD_REQUEST,
+                'massage': serializer.errors,
+                'id': None,
+            })
+        if status.HTTP_500_INTERNAL_SERVER_ERROR:
+            return Response({
+                'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
+                'massage': "Ошибка подключения к БД",
+                'id': None,
+            })
+
 
 
 
